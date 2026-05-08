c89efb06c090f315a49bffaade0f169332dced3f
Ghostdiabolico <ghostdiabolico@gmail.com>
2026-05-08 11:45:57 +1000

    Refactor socket.io connection handling
    Added console log for new device connection and organized event handling.

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const io = require('socket.io')(process.env.PORT || 3000, {
 });
 
 io.on('connection', (socket) => {
+  console.log('Nuovo dispositivo connesso');
+
+  // Gestione Reset
   socket.on('send_reset', () => {
     const newStartTime = Date.now();
     io.emit('receive_reset', newStartTime);
+  });
 
-io.on('connection', (socket) => {
-  // ... (codice offset)
-
-  // Quando riceve il comando toggle_fullscreen
+  // Gestione Fullscreen
   socket.on('send_fullscreen', () => {
     io.emit('receive_fullscreen');
-  });
-});
   });
 });
